071107390a0c51504acb02e6b661bb22154df276
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-12 23:32:37 +0900

    Secure the keys

--- nwitter/src/firebase.js
+++ nwitter/src/firebase.js
@@ -1,13 +1,13 @@
 import firebase from "firebase/app";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCcDZytKZX2zb7Sc5q2NPplHPztquMPb2M",
-    authDomain: "nwitter-f6a42.firebaseapp.com",
-    projectId: "nwitter-f6a42",
-    storageBucket: "nwitter-f6a42.appspot.com",
-    messagingSenderId: "174881702057",
-    appId: "1:174881702057:web:80526fc485486f5fdea9b8",
-    measurementId: "G-86S8B1FGDL"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+    appId: process.env.REACT_APP_APP_ID,
+    measurementId: process.env.REACT_APP_MEASUR_ID
 };
 
 export default firebase.initializeApp(firebaseConfig);
