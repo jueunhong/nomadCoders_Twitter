8a247328371ea1f2aeb3466cf06123aab9759d86
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-18 00:02:16 +0900

    Log Out

--- nwitter/src/components/Router.js
+++ nwitter/src/components/Router.js
@@ -4,16 +4,20 @@ import {
     Routes,
     Route,
 } from "react-router-dom";
-import Auth from "../routes/Auth";
-import Home from "../routes/Home";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "components/navigation";
 
 const AppRouter = ({isLoggedIn}) => {
     return(
         <Router>
+            {isLoggedIn && <Navigation />}
             <Routes>
                 {isLoggedIn ? (
                 <>
                 <Route path="/" element={<Home/>}/>
+                <Route path="/profile" element={<Profile/>}/>
                 </>) : (
                 <Route path="/" element={<Auth/>}/>
                 )}
