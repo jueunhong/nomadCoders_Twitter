94f7ca94f4a7eecf5ed0a1b95be403889b154853
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-20 21:48:12 +0900

    Save nweets text in Cloud Firestore

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,10 +1,17 @@
+import { dbService } from "fbase";
 import React from "react";
 import { useState } from "react";
 
 const Home = () => {
     const [nweet, setNweet] = useState("");
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
+        //document 생성
+        await dbService.collection("nweets").add({
+            nweet,
+            createdAt: Date.now()
+        });
+        setNweet("");
     };
     const onChange = (event) => {
         setNweet(event.target.value);
