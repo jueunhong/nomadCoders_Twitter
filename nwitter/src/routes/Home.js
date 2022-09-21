d157a5870339ee453ac0b69235c5bf434923c711
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-21 23:33:50 +0900

    Get Nweets

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,14 +1,30 @@
 import { dbService } from "fbase";
-import React from "react";
+import React, { useEffect } from "react";
 import { useState } from "react";
 
-const Home = () => {
+const Home = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
+    const [nweets, setNweets] = useState([]);
+    const getNweets = async () => {
+        const dbNweets = await dbService.collection("nweets").get();
+        dbNweets.forEach((document) => {
+            const nweetObject = {
+                ...document.data(),
+                id: document.id,
+            };
+            setNweets((prev) => [nweetObject, ...prev]);
+        });
+    };
+    
+    useEffect(() => {
+        getNweets();
+        }, []);
+
     const onSubmit = async (event) => {
         event.preventDefault();
         //document 생성
         await dbService.collection("nweets").add({
-            nweet,
+            text: nweet,
             createdAt: Date.now()
         });
         setNweet("");
@@ -22,6 +38,11 @@ const Home = () => {
             <input onChange={onChange} value={nweet} type="text" placeholder="What's on your mind" maxLength={120}/>
             <input type="submit" value="Nweet" />
         </form>
+        <div>
+            {nweets.map((nweet) => <div key={nweet.id}>
+                    <h4>{nweet.text}</h4>
+                </div>)}
+        </div>
     </div>)
 }
 
