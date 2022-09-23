58aba372856432dfc0c139f160d2abeece2b53b2
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-09-23 23:43:40 +0900

    Delete and Update Nweet

--- nwitter/src/routes/Home.js
+++ nwitter/src/routes/Home.js
@@ -1,3 +1,4 @@
+import Nweet from "components/Nweet";
 import { dbService } from "fbase";
 import React, { useEffect } from "react";
 import { useState } from "react";
@@ -35,9 +36,7 @@ const Home = ({ userObj }) => {
             <input type="submit" value="Nweet" />
         </form>
         <div>
-            {nweets.map((nweet) => <div key={nweet.id}>
-                    <h4>{nweet.text}</h4>
-                </div>)}
+            {nweets.map((nweet) => <Nweet key={nweet.id} nweetObj={nweet} isOwner={nweet.creatorId === userObj.uid}/>)}
         </div>
     </div>)
 }
