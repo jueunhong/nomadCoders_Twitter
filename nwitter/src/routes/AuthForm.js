04c4d58aebc5400974a8ad9afa51a9224a2571ad
jueunhong <85168145+jueunhong@users.noreply.github.com>
2022-10-07 23:48:39 +0900

    Cleaning Auth.js

---
+++ nwitter/src/routes/AuthForm.js
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { authService } from "fbase";
+
+const AuthForm = () => {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [newAccount, setNewAccount] = useState(true);
+    const [error, setError] = useState("");
+    const onChange = (event) => {
+        const {target: {name, value}} = event;
+        if (name === "email"){
+            setEmail(value);
+        } else if(name === "password") {
+            setPassword(value);
+        }
+    };
+    const onSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            if(newAccount) {
+            //create account
+            const data = await authService.createUserWithEmailAndPassword(
+                email, password);
+            console.log(data);
+            } else{
+            //login
+            const data = await authService.signInWithEmailAndPassword(
+                email, password);
+            console.log(data)
+            }
+            
+        } catch(error){
+            setError(error.message);
+        }
+    };
+    const toggleAccount = () => setNewAccount(prev => !prev);
+    return(
+    <>
+        <form onSubmit={onSubmit}>
+            <input 
+                name="email"
+                type="text"
+                placeholder="Email" 
+                required
+                value={email}
+                onChange={onChange} />
+            <input 
+                name="password"
+                type="password"
+                placeholder="Password"
+                required
+                value={password}
+                onChange={onChange} />
+            <input type="submit" value={newAccount ? "Create Account" : "Sign In"} />
+            {error}
+        </form>
+        <span onClick={toggleAccount}>{newAccount ? "Sign In" : "Create Account"}</span>
+    </>
+    )
+};
+
+export default AuthForm;
